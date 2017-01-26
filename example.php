341a74dd23cb34c13ec93a3d049fedecccab1bae
Sujeet Kumar <sujeetkv90@gmail.com>
2017-01-26 20:29:05 +0530

    updated script

--- example.php
+++ example.php
@@ -1,40 +1,53 @@
 <?php
-require './lib/Curl.php';
+require './src/Curl.php';
+require './src/CurlException.php';
+
+use SujeetKumar\CurlLib\Curl;
 
 $curl = new Curl(array(
-	'timeout' => 30, // time limit for request
-	'strict_mode' => false, // whether CURLOPT_FAILONERROR or not
-	'max_redirects' => 10, // number of redirections to follow
-	'http_version' => '1.1' // HTTP version (1.1, 1.0)
+    'timeout' => 30, // time limit for request
+    'strict_mode' => false, // whether CURLOPT_FAILONERROR or not
+    'max_redirects' => 10, // number of redirections to follow
+    'http_version' => '1.1' // HTTP version (1.1, 1.0)
 ));
 
 
 // simple GET request
 $res1 = $curl->get('http://example.com');
 //echo $res1;
-echo '<pre>'; print_r($curl->getInfo()); echo '</pre>';
+echo '<pre>';
+print_r($curl->getInfo());
+echo '</pre>';
 
 
 // simple POST request
-$res2 = $curl->post('http://example.com', array('name'=>'Sujeet', 'age'=>25), array(CURLOPT_TIMEOUT => 20));
+$res2 = $curl->post('http://example.com', array('name' => 'Sujeet', 'age' => 25), array(CURLOPT_TIMEOUT => 20));
 //echo $res2;
-echo '<pre>'; print_r($curl->getInfo()); echo '</pre>';
+echo '<pre>';
+print_r($curl->getInfo());
+echo '</pre>';
 
 
 // custom request
 $res3 = $curl->sendRequest('GET', 'http://example.com');
 //echo $res3;
-echo '<pre>'; print_r($curl->getInfo()); echo '</pre>';
+echo '<pre>';
+print_r($curl->getInfo());
+echo '</pre>';
 
 
 // custom request
 $res4 = $curl->setOptions(array(
-	CURLOPT_POST => true,
-	CURLOPT_POSTFIELDS => array('name'=>'Sujeet', 'age'=>25),
-	CURLOPT_TIMEOUT => 25
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => array('name' => 'Sujeet', 'age' => 25),
+    CURLOPT_TIMEOUT => 25
 ))->execute('http://example.com');
 //echo $res4;
-echo '<pre>'; print_r($curl->getInfo()); echo '</pre>';
+echo '<pre>';
+print_r($curl->getInfo());
+echo '</pre>';
 
 
-echo '<pre>'; print_r($curl->getHeaders('http://example.com')); echo '</pre>';
+echo '<pre>';
+print_r($curl->getHeaders('http://example.com'));
+echo '</pre>';
---
+++ src/CurlException.php
@@ -0,0 +1,13 @@
+<?php
+namespace SujeetKumar\CurlLib;
+
+/**
+ * CurlException class
+ * 
+ * @author Sujeet <sujeetkv90@gmail.com>
+ * @link https://github.com/sujeet-kumar/php-curl-lib
+ */
+class CurlException extends \Exception
+{
+    
+}
